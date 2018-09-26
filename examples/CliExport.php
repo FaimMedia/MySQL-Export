20d689e9593209e72075959229081a7577738ca2
Tim Peters (FaimMedia B.V.) <tim@faimmedia.nl>
2018-09-26 14:12:33 +0200

    Added CliExport example

---
+++ examples/CliExport.php
@@ -0,0 +1,30 @@
+#!/usr/bin/env php
+<?php
+
+use Phalcon\Db\Adapter\Pdo\Mysql;
+
+use FaimMedia\MySQLJSONExport\Export,
+    FaimMedia\MySQLJSONExport\Helper\Parameters;
+
+require dirname(__DIR__, 4) . '/vendor/autoload.php';
+
+$parameters = new Parameters();
+
+var_dump($parameters->getParameters());
+
+$parameters->validateRequired('host', 'database', 'username', 'password');
+
+$mysql = new Mysql([
+	'host'     => $parameters->getHost(),
+	'dbname'   => $parameters->getDatabase(),
+	'username' => $parameters->getUsername(),
+	'password' => $parameters->getPassword(),
+
+	'charset'  => 'utf8mb4',
+]);
+
+$export = new Export($mysql);
+
+var_dump($export->getTriggersArray());
+
+var_dump($export->getTablesArray());
